d66a82cc08730170a96ae24aefce852ca89c998c
chic-page-6502 <zacharytq@gmail.com>
2019-08-14 00:41:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailAddressParser
     @these_emails = emails
   end
 
-  def self.parse
+  def parse
     split_emails = @emails.split(/\s|[,]/)
     last_emails = split_emails.uniq
     last_emails
